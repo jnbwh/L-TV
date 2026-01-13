90905d8eae0c6537fb8e758fe7dd94dbcdc844ff
jnbwh <151704337+jnbwh@users.noreply.github.com>
2026-01-13 11:38:17 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -15,7 +15,7 @@ const PASSWORD_CONFIG = {
 const SITE_CONFIG = {
     name: 'LibreTV',
     url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
+    description: '学习强国',
     logo: 'image/logo.png',
     version: '1.0.3'
 };
